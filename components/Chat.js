1767b049aaeb0cb3250eb19c9faf4648f602aa86
Rokas <rookas.rudzenskas@gmail.com>
2021-05-25 06:42:42 +0300

    Rokas

--- components/Chat.js
+++ components/Chat.js
@@ -4,17 +4,23 @@ import getRecipientEmail from "../utils/getRecipientEmail";
 import {auth, db} from "../firebase";
 import {useAuthState} from "react-firebase-hooks/auth";
 import {useCollection} from "react-firebase-hooks/firestore";
+import {useRouter} from "next/router";
 
 const Chat = ({ id, users }) => {
     // console.log(id, users)
     const [user] = useAuthState(auth);
+    const router = useRouter();
     const [recipientSnapshot] = useCollection(db.collection('users').where('email', '===', getRecipientEmail(users, user)));
     // getting all the data from the recipient, and storing into the variable
     const recipient = recipientSnapshot?.docs?.[0]?.data();
+
     const recipientEmail = getRecipientEmail(users, user)
 
+    const enterChat = () => {
+        router.push(`/chat/${id}`);
+    }
     return (
-        <Container>
+        <Container onClick={enterChat}>
             {recipient ? (
                 <UserAvatar src={recipient?.photoURL} />
             ) : (
