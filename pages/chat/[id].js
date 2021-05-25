9bf10c008c768e8c9ebc09d23c623adc60002bfb
Rokas <rookas.rudzenskas@gmail.com>
2021-05-25 07:27:22 +0300

    Rokas

--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -2,8 +2,13 @@ import styled from "styled-components";
 import {Head} from "next/document";
 import Sidebar from "../../components/Sidebar";
 import ChatScreen from "../../components/ChatScreen";
+import {auth, db} from "../../firebase";
+import {useAuthState} from "react-firebase-hooks/auth";
 
-const Chat = () => {
+const Chat = ({chat, messages}) => {
+
+    const [user] = useAuthState(auth);
+
     return (
         <Container>
            <Sidebar />
@@ -17,6 +22,42 @@ const Chat = () => {
 };
 
 export default Chat;
+
+export async function getServerSideProps(context) {
+    // this all happens in server
+    // before you even see the page
+
+    // we are going where the the id is
+    const ref = db.collection("chats").doc(context.query.id);
+
+    // prep the messages on the server
+
+    const messageRes = await ref.collection("messages").orderBy("timestamp", "desc").get();
+
+    const messages = messageRes.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data(),
+    })).map(messages => ({
+        ...messages,
+        // will get normal time
+        timestamp: messages.timestamp.toDate().getTime(),
+    }));
+
+    // prep the chats
+
+    const chatRes = await ref.get();
+    const chat = {
+        id: chatRes.id,
+        ...chatRes.data(),
+    }
+
+    return {
+        props: {
+            messages: JSON.stringify(messages),
+            chat: chat,
+        },
+    };
+}
 
 const Container = styled.div`
   display: flex;
